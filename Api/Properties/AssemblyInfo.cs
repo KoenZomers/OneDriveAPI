481869481650754b648d3ece0d99ebb1a3b27164
Koen Zomers <mail@koenzomers.nl>
2015-06-19 16:48:03 +0200

    Updated assembly information and switched on version auto incrementing

--- Api/Properties/AssemblyInfo.cs
+++ Api/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BusinessLogic")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OneDrive API by Koen Zomers")]
+[assembly: AssemblyDescription("API which allows easy communication with OneDrive")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BusinessLogic")]
+[assembly: AssemblyCompany("Koen Zomers")]
+[assembly: AssemblyProduct("OneDrive API by Koen Zomers")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
